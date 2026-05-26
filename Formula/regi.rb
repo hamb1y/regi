88c07a00e4e6e3f6a5c953b966f1b3b163bd31d4
hamb1y <rishi.s.malnad@gmail.com>
2026-05-26 13:33:44 +0530

    bump formula to v0.2.1

--- Formula/regi.rb
+++ Formula/regi.rb
@@ -1,8 +1,8 @@
 class Regi < Formula
   desc "Tiny newline-delimited plaintext register CLI"
   homepage "https://github.com/hamb1y/regi"
-  url "https://github.com/hamb1y/regi/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "0b39e6835807cb814aef9877103db1d355ef6af5060cce6c6d55150a83dc543d"
+  url "https://github.com/hamb1y/regi/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "371dc658b7871b0a35fd11243b34e3c3ee79253c53d36f407881f4571f54fd11"
   head "https://github.com/hamb1y/regi.git", branch: "main"
   license "BSD-3-Clause"
 
